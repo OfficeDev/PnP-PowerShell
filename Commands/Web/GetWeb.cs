511d847e94239b4cdb35d67f6b1943f8f9c8debb
Koen Zomers <koen@zomers.eu>
2017-07-31 19:49:33 +0200

    Added description and samples

--- Commands/Web/GetWeb.cs
+++ Commands/Web/GetWeb.cs
@@ -7,10 +7,20 @@ using SharePointPnP.PowerShell.CmdletHelpAttributes;
 namespace SharePointPnP.PowerShell.Commands
 {
     [Cmdlet(VerbsCommon.Get, "PnPWeb")]
-    [CmdletHelp("Returns the current web object",
+    [CmdletHelp("Returns a web object",
         Category = CmdletHelpCategory.Webs,
+        DetailedDescription = "This allows returning a web object representing either the current context its web or one of the webs located underneath it"
         OutputType = typeof(Web),
         OutputTypeLink = "https://msdn.microsoft.com/en-us/library/microsoft.sharepoint.client.web.aspx")]
+    [CmdletExample(Code = @"PS:> Get-PnPWeb",
+                Remarks = @"Returns the web of the current context",
+                SortOrder = 1)]
+    [CmdletExample(Code = @"PS:> Get-PnPWeb -Identity aa66f67e-46c0-4474-8a82-42bf467d07f2",
+                Remarks = @"Returns the current web or a subweb with the Id 'aa66f67e-46c0-4474-8a82-42bf467d07f2'",
+                SortOrder = 2)]
+    [CmdletExample(Code = @"PS:> Get-PnPWeb -Identity ""HR""",
+                Remarks = @"Returns the subsite located at /HR",
+                SortOrder = 3)]
     public class GetWeb : PnPRetrievalsCmdlet<Web>
     {
         [Parameter(Mandatory = false, ValueFromPipeline = true, Position = 0)]
