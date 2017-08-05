72d598f7bbb30338a7973d6f38587fd34f3082a3
Koen Zomers <koen@zomers.eu>
2017-08-05 15:03:45 +0200

    Added -UpdateExistingLists flag to update the field in existing lists as well. Fixed bug in piping Get-PnPField to Set-PnPField. Updated help text to express that field names are case sensitive.

--- Commands/Fields/SetField.cs
+++ Commands/Fields/SetField.cs
@@ -14,22 +14,29 @@ namespace SharePointPnP.PowerShell.Commands.Fields
         OutputTypeLink = "https://msdn.microsoft.com/en-us/library/microsoft.sharepoint.client.field.aspx")]
     [CmdletExample(
         Code = @"PS:> Set-PnPField -Identity AssignedTo -Values @{JSLink=""customrendering.js"";Group=""My fields""}",
-        Remarks = @"Updates the AssignedTo field on the current web to use customrendering.js for the JSLink and sets the group name the field is categorized in to ""My Fields""",
+        Remarks = @"Updates the AssignedTo field on the current web to use customrendering.js for the JSLink and sets the group name the field is categorized in to ""My Fields"". Lists that are already using the AssignedTo field will not be updated.",
         SortOrder = 1)]
+    [CmdletExample(
+        Code = @"PS:> Set-PnPField -Identity AssignedTo -Values @{JSLink=""customrendering.js"";Group=""My fields""} -UpdateExistingLists",
+        Remarks = @"Updates the AssignedTo field on the current web to use customrendering.js for the JSLink and sets the group name the field is categorized in to ""My Fields"". Lists that are already using the AssignedTo field will also be updated.",
+        SortOrder = 2)]
     [CmdletExample(
         Code = @"PS:> Set-PnPField -List ""Tasks"" -Identity ""AssignedTo"" -Values @{JSLink=""customrendering.js""}",
         Remarks = @"Updates the AssignedTo field on the Tasks list to use customrendering.js for the JSLink",
-        SortOrder = 2)]
+        SortOrder = 3)]
     public class SetField : PnPWebCmdlet
     {
         [Parameter(Mandatory = false, ValueFromPipeline = true, HelpMessage = "The list object, name or id where to update the field. If omited the field will be updated on the web.")]
         public ListPipeBind List;
 
-        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, HelpMessage = "The field object, internal field name or field id to update")]
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, HelpMessage = "The field object, internal field name (case sensitive) or field id to update")]
         public FieldPipeBind Identity = new FieldPipeBind();
 
         [Parameter(Mandatory = true, HelpMessage = "Hashtable of properties to update on the field. Use the syntax @{property1=\"value\";property2=\"value\"}.")]
         public Hashtable Values;
+
+        [Parameter(Mandatory = false, HelpMessage = "If provided, the field will be updated on existing lists that use it as well. If not provided or set to $false, existing lists using the field will remain unchanged but new lists will get the updated field.")]
+        public SwitchParameter UpdateExistingLists;
 
         protected override void ExecuteCmdlet()
         {
@@ -66,6 +73,11 @@ namespace SharePointPnP.PowerShell.Commands.Fields
                 {
                     field = ClientContext.Web.Fields.GetByInternalNameOrTitle(Identity.Name);
                 }
+                else if(Identity.Field != null)
+                {
+                    field = Identity.Field;
+                }
+
                 if(field == null)
                 {
                     throw new ArgumentException("Unable to retrieve field with id, name or the field instance provided through Identity on the current web", "Identity");
@@ -96,7 +108,7 @@ namespace SharePointPnP.PowerShell.Commands.Fields
                     }
                 }
             }
-            field.Update();
+            field.UpdateAndPushChanges(UpdateExistingLists);
             ClientContext.ExecuteQueryRetry();
         }
     }
